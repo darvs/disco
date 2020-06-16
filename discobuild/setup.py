afaeb89c12173000849d8741883d77fb22d82894
Player One <steve@darvs.net>
2020-06-15 20:55:53 -0400

    - Correct formatting errors

--- discobuild/setup.py
+++ discobuild/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys,yaml,re
+import sys, yaml, re
 
 with open('/tmp/disco.yaml') as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
@@ -11,16 +11,6 @@ with open('/tmp/disco.yaml') as f:
     packages = data['packages']
     build = data['build']
 
-    # with open('/tmp/build', 'w') as b:
-        # project=sys.argv[1]
-        # repo=data['repo']['url']
-        # build=data['build']
-        # b.write('#!/usr/bin/env bash\n')
-        # b.write('cd /projects\n')
-        # b.write(f'[ -d {project} ] || git clone --recursive {repo}\n')
-        # b.write(f'cd {project}\n')
-        # b.write(f'{build}\n')
-
     with open('/tmp/Dockerfile', 'w') as d:
         def writeln(s):
             d.write(s + '\n')
@@ -29,8 +19,8 @@ with open('/tmp/disco.yaml') as f:
             writeln('  echo ' + re.escape(s) + '>> /tmp/build && \\')
             
         writeln('FROM disco')
-        writeln('RUN \\\n')
-        writeln(f'  DEBIAN_FRONTEND=noninteractive apt install -y {packages}\n')
+        writeln('RUN \\')
+        writeln(f'  DEBIAN_FRONTEND=noninteractive apt install -y {packages}')
 
         writeln('RUN \\')
         writeln('  [ ! -f /tmp/build ] || rm /tmp/build && \\')
@@ -40,9 +30,10 @@ with open('/tmp/disco.yaml') as f:
         writebuild('cd /projects')
         writebuild(f'[ -d {project} ] || git clone --recursive {repo}')
         writebuild(f'cd {project}')
-        #d.write(f' echo {build}\n >>/tmp/build & \\\n')
+
         for s in build.split('\n'):
-            writebuild(s)  
-        writeln(f'  echo I compiled {project}')
-        writeln('RUN cat /tmp/build')
+            writebuild(s)
+        writeln(f'  chmod a+x /tmp/build');
+
+
 
